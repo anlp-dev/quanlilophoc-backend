d1a1264fc846d1b79b33888573924988f65415b6
anlp-dev <anlp.sep19@gmail.com>
2024-12-11 11:07:10 +0700

    fix deploy v3

--- functions/server.js
+++ functions/server.js
@@ -6,8 +6,8 @@ const router = require('../src/routes/main/MainRoutes');
 const { connect } = require('../src/database/db');
 const {logRequest} = require('../src/middleware/rateLimiter')
 const cors = require('cors');
-const port = process.env.PORT || 8888;
-const hostname = process.env.HOST_NAME || 'localhost';
+// const port = process.env.PORT || 8888;
+// const hostname = process.env.HOST_NAME || 'localhost';
 const security = require('../src/configs/security');
 
 require('../src/configs/auth');
@@ -17,6 +17,14 @@ connect();
 configViewEngine(app);
 security(app);
 app.use(router)
-app.listen(port, hostname, () => {
-  console.log(`App listening at http://${hostname}:${port}`)
-})
+// app.listen(port, hostname, () => {
+//   console.log(`App listening at http://${hostname}:${port}`)
+// })
+
+const ServerlessHttp = require('serverless-http');
+const handler = ServerlessHttp(app);
+
+module.exports.handler = async (event, context) => {
+  const res = await handler(event, context);
+  return res;
+};
